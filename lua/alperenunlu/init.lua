a2b5bea9438a0a1b4469ba6729ba330c1dbbc46a
alperenunlu <alperenunlu@Alperens-MBP.local>
2024-05-08 04:09:02 +0300

    Initial commit

---
+++ lua/alperenunlu/init.lua
@@ -0,0 +1,11 @@
+-- [[ Setting options ]]
+require 'alperenunlu.configure.options'
+
+-- [[ Basic Keymaps ]]
+require 'alperenunlu.configure.keymaps'
+
+-- [[ Install `lazy.configure.nvim` plugin manager ]]
+require 'alperenunlu.configure.lazy-bootstrap'
+
+-- [[ Configure and install plugins ]]
+require 'alperenunlu.configure.lazy-plugins'
---
+++ lua/alperenunlu/plugins/custom/lsp-signature.lua
@@ -0,0 +1,16 @@
+return {
+  'ray-x/lsp_signature.nvim',
+  opts = {
+    -- floating_window_off_x = 1,
+    -- floating_window_off_y = -2,
+
+    floating_window = false,
+    toggle_key = '<C-s>',
+    toggle_key_flip_floatwin_setting = true,
+    -- extra_trigger_chars = { ',', '=' },
+
+    hint_prefix = '😸 ',
+    -- auto_close_after = 1,
+    -- close_timeout = 1,
+  },
+}
